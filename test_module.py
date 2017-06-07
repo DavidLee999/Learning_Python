20a0f5eb512b49f4034dfbf18d4834d2de9bec77
PenghuaLI <MoonLee003@outlook.com>
2017-06-07 11:02:35 +0200

    module

---
+++ test_module.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 07 09:19:08 2017
+
+@author: li_pe
+"""
+
+' a test module'
+
+__author__  = 'Li'
+
+import sys
+
+def test():
+    
+    args = sys.argv
+    
+    if len(args) == 1:
+        
+        print 'Hello, world.'
+        
+    if len(args) == 2:
+        
+        print 'Hello, %s.' % args[1]
+        
+    else:
+        
+        print 'too much arguments.'
+        
+if __name__ == '__main__':
+    
+    test()
