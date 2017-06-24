c6769fa0c3dfa1eebff5eaf18cef833e31c32574
DavidLee999 <MoonLee003@outlook.com>
2017-06-24 23:55:14 +0200

    permute

--- permute.py
+++ permute.py
@@ -24,4 +24,54 @@ def permut( s ):
 
     permute( s, 0, len(s) - 1 )
     
-permut('abcd')
+def permu(s):
+
+    s_copy = list(s)
+    if s_copy:
+        results = []
+    
+        for ele in s_copy:
+        
+            temp = s_copy[:]
+        
+            temp.remove(ele)
+        
+            for res in permu(temp):
+            
+                results.append([ele] + res)
+    
+        return results
+        
+    else:
+        
+        return [[]]
+    
+    
+a = permu('abc')
+
+print(a)
+# def get_all_permu(some_list):
+
+    # if some_list:
+
+        # all_p = []   #定义全部排列为一个列表
+
+        # for x in some_list:  #第一次遍历：遍历列表中的所有元素
+
+            # temp = some_list[:]
+
+            # temp.remove(x)  #得到不包含x的列表
+
+            # for y in get_all_permu(temp):  
+
+            # #第二次遍历：遍历不包含x的所有元素的组合，这也是递归实施的地方
+
+                # all_p.append([x] + y)  
+
+               # #将x与不包含x的其他元素的所有组合放在一起，组成一个原列表的排列
+
+        # return all_p
+
+    # else:
+
+        # return [[]] 
